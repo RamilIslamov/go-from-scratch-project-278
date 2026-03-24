a7c652f0d381dedc6782ec9033745990893e0b02
Ramil <iramil73@gmail.com>
2026-03-24 20:32:08 +0300

    add render deploy and sentry

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
 echo "[run.sh] Starting service"
 
 echo "[run.sh] Running DB migrations"
-goose -dir ./db/migrations postgres "${DATABASE_URL}" up
+/bin/goose -dir ./db/migrations postgres "${DATABASE_URL}" up
 
 echo "[run.sh] Starting Go app"
 exec /app/bin/app
